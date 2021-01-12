a30954fe4ae8d63dccff448536fb0f619829df7c
Moustafa-Ahmed-2020 <60446868+Moustafa-Ahmed-2020@users.noreply.github.com>
2021-01-13 00:44:25 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Weather-Journal App Project
+
+## Overview
+
+This is my attempt for the weather app project in the Udacity Web devlopment Professional Nanodegree
+
+## Cahnges
+
+- 13/1/2021
+  First submission
---
+++ server.js
@@ -0,0 +1,59 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 8000;
+
+const listening = function() {
+    console.log(`Server is up and running on ${port}`);
+}
+
+const server = app.listen(port, listening);
+
+
+const weatherData = [];
+
+//Create a POST route
+const postInfo = function(req,res) {
+    console.log(req.body);
+    newEntry = {
+        name: req.body.name,
+        date: req.body.date,
+        current: req.body.current,
+        min: req.body.min,
+        max: req.body.max,
+        sky: req.body.sky,
+        feel: req.body.feels
+    }
+    weatherData.push(newEntry);
+    res.send(weatherData);
+}
+
+
+app.post('/weather',postInfo);
+
+//Create a GET request function
+
+const getData = function(req,res) {
+    res.send(weatherData);
+}
+
+app.get('/all',getData);
